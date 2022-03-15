b1f962b9a1bc62c3a28dac355529696d70c6cb86
JordanTanReiYao <JTAN418@e.ntu.edu.sg>
2022-03-15 21:50:27 +0800

    Classify sentiment

--- backend/insertData.py
+++ backend/insertData.py
@@ -2,14 +2,14 @@ import pandas as pd
 import pysolr
 
 
-tweets = pd.read_csv('CZ4034_Tweets.csv', dtype={'id': str,'following':int,'followers':int,"totaltweets":int,
+tweets = pd.read_csv('CZ4034_Labelled_Tweets.csv', dtype={'id': str,'following':int,'followers':int,"totaltweets":int,
                                                 "retweetcount":int,"favoritecount":int})
 
 
 data=[{"id":index,"username":row['username'],"acctdesc":row['acctdesc'],"location":row['location'],
 "following":row["following"],"followers":row["followers"],"totaltweets":row["totaltweets"],
 "usercreatedts":row['usercreatedts'],"tweetcreatedts":row["tweetcreatedts"],"retweetcount":row["retweetcount"],
-"favoritecount":row["favoritecount"],"text":row["text"],} for index,row in tweets.iterrows()]
+"favoritecount":row["favoritecount"],"text":row["text"],'sentiment':row['sentiment']} for index,row in tweets.iterrows()]
 
 solr = pysolr.Solr("http://localhost:8983/solr/tweets", always_commit=True)
 print("Add data to Solr:")
