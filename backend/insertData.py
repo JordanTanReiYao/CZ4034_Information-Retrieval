08d524b3c1ca70ffd08a3e1477c912a869ffb5de
JordanTanReiYao <JTAN418@e.ntu.edu.sg>
2022-03-27 23:37:19 +0800

    Minor changes

--- backend/insertData.py
+++ backend/insertData.py
@@ -3,13 +3,14 @@ import pysolr
 
 
 tweets = pd.read_csv('CZ4034_Labelled_Tweets.csv', dtype={'id': str,'following':int,'followers':int,"totaltweets":int,
-                                                "retweetcount":int,"favoritecount":int})
+                                                "retweetcount":int,"favoritecount":int,"acctdesc":str})
 
 
-data=[{"id":index,"username":row['username'],"image_url":row['image_url'],"acctdesc":row['acctdesc'],"location":row['location'],
+data=[{"id":index,"username":row['username'],"image_url":row['image_url'],"acctdesc":row['acctdesc'] ,"location":row['location'],
 "following":row["following"],"followers":row["followers"],"totaltweets":row["totaltweets"],
 "usercreatedts":row['usercreatedts'],"tweetcreatedts":row["tweetcreatedts"],"retweetcount":row["retweetcount"],
 "favoritecount":row["favoritecount"],"text":row["text"],'sentiment':row['sentiment']} for index,row in tweets.iterrows()]
+
 
 solr = pysolr.Solr("http://localhost:8983/solr/tweets", always_commit=True)
 print("Add data to Solr:")
